46e0d4a7bff3eadf6704c5b4573080f0e181b642
Yixiao <1411101569@qq.com>
2023-01-26 20:12:23 +0000

    bug:  remain problems for coordinates

--- model.js
+++ model.js
@@ -1,25 +1,59 @@
 window.onload = function() {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const fov = 75;
+    const aspect = window.innerWidth / window.innerHeight;  // 相机默认值
+    const near = 5;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(2, 2, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0xff00});
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    let material = new THREE.MeshPhongMaterial({color: 0xff00});
+    let geometry = new THREE.BoxGeometry(1, 6, 1);
+    
+    geometry.translate(0,-3,0);
+    const body = new THREE.Mesh(geometry, material);
 
-    camera.position.z = 5;
+    // geometry.translate(0,0,0);
+    const left_hand = new THREE.Mesh(geometry, material);
+    // left_hand.position.set(0,3,0);
+    left_hand.rotation.x=0;
+    left_hand.rotation.z=-1;
+
+    const right_hand = new THREE.Mesh(geometry, material);
+    // left_hand.position.set(0,3,0);
+    right_hand.rotation.x=0;
+    right_hand.rotation.z=1;
+    
+    scene.add(body);
+    body.add(left_hand);
+    body.add(right_hand);
+    
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+    camera.position.set(0, 5, 10);
+    camera.lookAt(0, 0, 0);
+    let tmp=2*Math.PI/8, k=0;
 
     function animate() {
+ 
         requestAnimationFrame(animate);
-
-        // cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
-        // cube.rotation.z += 0.01;
-
+        
+        if (k > 4) {
+            left_hand.rotation.x -=0.3;
+            right_hand.rotation.x +=0.3;
+            k=0;
+        }else {
+            k+=1;
+        }
+        
         renderer.render(scene, camera);
     };
 
